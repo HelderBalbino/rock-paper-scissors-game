0f17bad344c0a0f6d65d67cbaf35a469f3eded3d
Helder Balbino <helderbalbino@googlemail.com>
2023-01-05 16:36:41 +0000

    corret array random formula

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ let choices = ['r', 'p', 's'];
 
 let userChoice = prompt('please enter r, p, or s to play the game');
 
-let computerChoice = choices[Math.floor(Math.random() * 3)];
+let computerChoice = choices[Math.floor(Math.random() * choices.length)];
